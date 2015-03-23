5e01b60930129e1b840ff375ee1d98bc0cd5f428
Ronald Hernandez <2ronald.hernandez@gmail.com>
2015-03-23 10:48:56 -0700

    Initial Commit

---
+++ Superheropedia/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Superheropedia
+//
+//  Created by Ronald Hernandez on 3/23/15.
+//  Copyright (c) 2015 Ron. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Superheropedia/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Superheropedia
+//
+//  Created by Ronald Hernandez on 3/23/15.
+//  Copyright (c) 2015 Ron. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
