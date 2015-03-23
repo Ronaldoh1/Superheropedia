94bf63327b885357437d9ae4c26d3de94f618b67
Ronald Hernandez <2ronald.hernandez@gmail.com>
2015-03-23 12:49:58 -0700

    initial commit - used api call to pull data for superheros and thier corresponding images and display them on a table view

--- Superheropedia/ViewController.m
+++ Superheropedia/ViewController.m
@@ -8,7 +8,10 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController ()<UITableViewDataSource, UITableViewDelegate>
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+
+@property NSArray *superheroArray;
 
 @end
 
@@ -17,11 +20,39 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+
+//    self.superheroArray = [NSArray arrayWithObjects:[NSDictionary dictionaryWithObjectsAndKeys:@"Batman",@"name", @"28", @"age",nil], [NSDictionary dictionaryWithObjectsAndKeys:@"Ironman",@"name", @"34", @"age", nil], nil];
+
+    NSURL *url =[NSURL URLWithString:@"https://s3.amazonaws.com/mobile-makers-lib/superheroes.json"];
+
+    NSURLRequest *request =[NSURLRequest requestWithURL:url];
+    [NSURLConnection sendAsynchronousRequest:request queue:[NSOperationQueue mainQueue] completionHandler:^(NSURLResponse *response, NSData *data, NSError *connectionError) {
+        self.superheroArray = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingAllowFragments error:nil];
+        
+        [self.tableView reloadData];
+
+         }];
+
+
+
+}
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cellID"];
+
+    NSDictionary *dictionary = [self.superheroArray objectAtIndexedSubscript:indexPath.row];
+
+    cell.textLabel.text = [dictionary objectForKey:@"name"];
+    cell.detailTextLabel.text = [dictionary objectForKey:@"description"];
+    cell.detailTextLabel.numberOfLines = 0;
+    NSURL *url = [NSURL URLWithString:[dictionary objectForKey:@"avatar_url"]] ;
+    cell.imageView.image = [UIImage imageWithData:[NSData dataWithContentsOfURL:url]];
+
+    return cell;
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return self.superheroArray.count;
 }
+
 
 @end
